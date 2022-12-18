8340f1701b7135a47a93954fb057a9af679d3361
Inna Belkina <inna0311@inbox.ru>
2022-12-18 16:28:14 +0600

    Программа для четвертой задачи

---
+++ HomeworkC#_4/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int a = 2; a <= N; a += 2) Console.WriteLine(a);
+
+
